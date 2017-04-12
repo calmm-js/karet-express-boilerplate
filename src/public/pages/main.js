8937fd00f86c1a91d84ca9aba39604a15bbdbe3e
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-04-12 21:48:07 +0300

    Saner contacts example

--- src/public/pages/main.js
+++ src/public/pages/main.js
@@ -1,36 +1,33 @@
-import * as R from "ramda"
 import * as L from "partial.lenses"
 import * as U from "karet.util"
 import React  from "karet"
 
-const TextInput = ({value}) => console.log("TextInput") ||
+const TextInput = ({value}) =>
   <input type="text"
          value={value}
          onChange={U.getProps({value})}/>
 
-const Contact = ({contact}) => console.log("Contact") ||
+const Contact = ({contact}) =>
   <div>
     <TextInput value={U.view("name", contact)}/>
     <TextInput value={U.view("phone", contact)}/>
     <button onClick={() => contact.remove()}>Remove</button>
   </div>
 
-const Contacts = ({contacts}) => console.log("Contacts") ||
+const Contacts = ({contacts}) =>
   <div>
-    {U.seq(contacts,
-           U.mapElems((contact, i) =>
-                      <Contact key={i} contact={contact}/>))}
+    <button onClick={() => contacts.modify(U.append({name: "", phone: ""}))}>
+      Add contact
+    </button>
+    <div>
+      {U.seq(contacts,
+             U.mapElems((contact, i) => <Contact key={i} contact={contact}/>))}
+    </div>
+    Number of contacts: {U.length(contacts)}
   </div>
 
-export default U.withContext((_, {state}) => {
-  const contacts = U.view(L.define([]), state)
-  state.log("state")
-  return <div>
+export default U.withContext((_, {state}) =>
+  <div>
     <h1>Main page</h1>
-    <button onClick={() =>
-              state.modify(R.append({name: "", phone: ""}))}>
-      Add</button>
-    <Contacts contacts={contacts}/>
-    Num: {U.length(contacts)}
-  </div>
-})
+    <Contacts contacts={U.view(["contacts", L.define([])], state)}/>
+  </div>)
