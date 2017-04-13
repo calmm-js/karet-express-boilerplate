90158bbf381f3eb5ad908cd1bb4336fc1b351bca
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-04-13 16:33:12 +0300

    Show query parameters

--- src/public/pages/another-page.js
+++ src/public/pages/another-page.js
@@ -1,6 +1,11 @@
-import React from "karet"
+import * as U from "karet.util"
+import React  from "karet"
 
-export default () =>
+import Link from "../components/link"
+
+export default U.withContext((_, {params}) =>
   <div>
     <h1>Another page</h1>
-  </div>
+    <div>{U.lift(x => JSON.stringify(x))(params)}</div>
+    <Link href="/another-page?hello=world">Hello!</Link>
+  </div>)
