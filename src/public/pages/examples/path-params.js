73731c677e91e454c6744bc6cb13148e42c29a37
Kurt Milam <kurt.milam@xioup.com>
2017-04-24 20:22:13 +0200

    Split getter and setter functions out of subPathL
    Also improved the pathRoot regex and suppressed the ability to add a
    forward slash at the beginning of the PathInput.

--- src/public/pages/examples/path-params.js
+++ src/public/pages/examples/path-params.js
@@ -6,19 +6,25 @@ import { PathInput }       from "../../components/restricted-input"
 import { PrettyStringify } from "../../components/pretty-stringify"
 
 const getPagePathRoot =
-  U.pipe( x => U.match( /(^\/[^/]+\/).*/, x )[ 1 ] )
+  U.pipe( x => U.match( /(^\/[^/]+\/?).*/, x )[ 1 ] )
+
+const subPathGetter = pagePathRoot =>
+  U.pipe( U.replace( pagePathRoot, '' )
+                   , decodeURIComponent
+                   )
+const subPathSetter = pagePathRoot =>
+  U.pipe( U.map( U.when( x => x !== '/'
+                       , encodeURIComponent
+                       )
+               )
+        , U.join( '' )
+        , U.concat( pagePathRoot )
+        , U.replace( pagePathRoot + '/', pagePathRoot )
+        )
 
 const _subPathL = pagePathRoot =>
-  L.iso( U.pipe( U.replace( pagePathRoot, '' )
-               , decodeURIComponent
-               )
-       , U.pipe( U.map( U.when( x => x !== '/'
-                              , encodeURIComponent
-                              )
-                      )
-               , U.join( '' )
-               , U.concat( pagePathRoot )
-               )
+  L.iso( subPathGetter( pagePathRoot )
+       , subPathSetter( pagePathRoot )
        )
 const subPathL = U.lift( _subPathL )
 
