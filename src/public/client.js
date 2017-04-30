44d8183f2253848d0cfe955d76c832c48a96d1d5
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-04-30 21:50:56 +0300

    Expose RPC and Request modules in development mode

--- src/public/client.js
+++ src/public/client.js
@@ -8,6 +8,9 @@ import ReactDOM   from "react-dom"
 import MaybeSessionStored from "../client/maybe-session-stored"
 import {location}         from "../client/window"
 
+import * as Request from "../client/request"
+import * as RPC     from "../client/rpc"
+
 import Page from "./components/page"
 
 import * as Meta  from "./meta"
@@ -36,10 +39,12 @@ if (process.env.NODE_ENV !== "production") {
   window.Kefir = Kefir
   window.L = L
   window.R = R
+  window.RPC = RPC
   window.React = React
+  window.Request = Request
   window.U = U
-  window.state = state
   window.context = context
+  window.state = state
 
   state.log("state")
 }
