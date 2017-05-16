63ce1e373f7f4bcbbdbf17953b7afacfd9ea23f0
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-05-16 10:34:22 +0300

    A couple of extra passes reduces bundle size further

--- rollup.config.js
+++ rollup.config.js
@@ -43,7 +43,10 @@ export default {
       }
     }),
     babel(),
-    process.env.NODE_ENV === "production" &&
-      uglify()
+    process.env.NODE_ENV === "production" && uglify({
+      compress: {
+        passes: 3
+      }
+    })
   ].filter(x => x)
 }
