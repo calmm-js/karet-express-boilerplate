e8c5769cc092f083432a226bdce0c71c5c5e7362
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-08-01 12:22:26 +0300

    Updated deps and using node-globals rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,16 @@
-import babel       from "rollup-plugin-babel"
-import builtins    from "rollup-plugin-node-builtins"
-import commonjs    from "rollup-plugin-commonjs"
-import nodeResolve from "rollup-plugin-node-resolve"
-import replace     from "rollup-plugin-replace"
-import uglify      from "rollup-plugin-uglify"
+import babel        from "rollup-plugin-babel"
+import commonjs     from "rollup-plugin-commonjs"
+import nodeBuiltins from "rollup-plugin-node-builtins"
+import nodeGlobals  from "rollup-plugin-node-globals"
+import nodeResolve  from "rollup-plugin-node-resolve"
+import replace      from "rollup-plugin-replace"
+import uglify       from "rollup-plugin-uglify"
 
 export default {
   plugins: [
     replace({
-      "global.setTimeout": "window.setTimeout",
-      "global.clearTimeout": "window.clearTimeout",
-      "global.performance": "window.performance",
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     }),
-    builtins(),
     nodeResolve(),
     commonjs({
       include: 'node_modules/**',
@@ -43,6 +40,8 @@ export default {
       }
     }),
     babel(),
+    nodeGlobals(),
+    nodeBuiltins(),
     process.env.NODE_ENV === "production" && uglify({
       compress: {
         passes: 3
