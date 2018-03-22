ec82089860b54065b07bedaf531d080856f4c350
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-03-22 08:23:09 +0200

    Simplified and also passes `path` to components

--- src/public/components/router.js
+++ src/public/components/router.js
@@ -1,31 +1,29 @@
+import * as L from 'partial.lenses'
 import * as R from 'ramda'
 import * as React from 'karet'
 import * as U from 'karet.util'
 import toRegex from 'path-to-regexp'
 
-// TODO: Make it possible to add routes more than once?
+const expandRoutes = L.collectAs((Component, route) => {
+  const params = []
+  const regex = toRegex(route, params)
+  return {
+    route,
+    regex,
+    keys: ['path'].concat(params.map(L.get('name'))),
+    Component
+  }
+}, L.values)
 
-// begin routes initialization
-const expandRoutes = R.pipe(R.toPairs, R.map(R.zipObj(['route', 'Component'])))
+const sortStaticFirst = R.sortBy(({keys}) => (keys.length === 1 ? 0 : 1))
 
-const addRegexes = R.map(entry => {
-  const keys = []
-  const regex = toRegex(entry.route, keys)
-  regex.keys = keys.map(R.prop('name'))
-  return {...entry, regex}
-})
+const prepareRoutes = R.o(sortStaticFirst, expandRoutes)
 
-const sortStaticFirst = R.sortBy(({regex}) => (regex.keys.length === 0 ? 0 : 1))
-
-const prepareRoutes = R.pipe(expandRoutes, addRegexes, sortStaticFirst)
-// end routes initialization
-
-// route resolution
 const router = U.lift((routes, NotFound, path) => {
   for (let i = 0; i < routes.length; ++i) {
-    const {Component, regex} = routes[i]
+    const {Component, keys, regex} = routes[i]
     const match = regex.exec(path)
-    if (match) return <Component {...R.zipObj(regex.keys, R.tail(match))} />
+    if (match) return <Component {...R.zipObj(keys, match)} />
   }
   return <NotFound />
 })
