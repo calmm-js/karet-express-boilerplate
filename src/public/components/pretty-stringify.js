8150bac28abe04acb13626e9a927cd2ed8121c91
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-03-22 08:21:47 +0200

    Use lifted `stringify` from Karet.Util

--- src/public/components/pretty-stringify.js
+++ src/public/components/pretty-stringify.js
@@ -2,7 +2,5 @@ import * as React from 'karet'
 import * as U from 'karet.util'
 
 export const PrettyStringify = ({value, replacer = null, space = 2}) => (
-  <pre className="pretty-stringify">
-    {U.lift(JSON.stringify)(value, replacer, space)}
-  </pre>
+  <pre className="pretty-stringify">{U.stringify(value, replacer, space)}</pre>
 )
